2e92303d1d699d2a80f741d37b0c4d453d50b926
Muntasir Udoy <muntasirudoy4@gmail.com>
2024-10-08 12:46:49 +0600

    update

--- app/(static)/(components)/ui/header.tsx
+++ app/(static)/(components)/ui/header.tsx
@@ -15,7 +15,7 @@ export function Header() {
   return (
     <>
       <div className=" bg-brand-100 py-3 md:py-4 lg:py-5">
-        <div className="container flex justify-between items-center">
+        <div className="container flex justify-between  items-center">
           <Link href={"/"}>
             <Image
               className="max-w-[120px] md:max-w-[150px] lg:max-w-[180px]"
@@ -26,25 +26,25 @@ export function Header() {
               loading="eager"
             />
           </Link>
-          <div className=" hidden md:flex gap-6 items-center">
-            <ul className="flex gap-5 items-center  font-semibold">
-              <NavLink href="/" activeClassName="active-link">
-                Home
-              </NavLink>
-              <NavLink href="/about" activeClassName="active-link">
-                Company
-              </NavLink>
-              <NavLink href="/service" activeClassName="active-link">
-                Services
-              </NavLink>
-              <NavLink href="/blogs" activeClassName="active-link">
-                Blogs
-              </NavLink>
-            </ul>
-            <Link href={"/contact"}>
-              <PrimaryBtn>Contact Now</PrimaryBtn>
-            </Link>
-          </div>
+
+          <ul className="md:flex hidden gap-10 items-center  font-semibold">
+            <NavLink href="/" activeClassName="active-link">
+              Home
+            </NavLink>
+            <NavLink href="/about" activeClassName="active-link">
+              Company
+            </NavLink>
+            <NavLink href="/service" activeClassName="active-link">
+              Services
+            </NavLink>
+            <NavLink href="/blogs" activeClassName="active-link">
+              Blogs
+            </NavLink>
+          </ul>
+          <Link className="md:block hidden" href={"/contact"}>
+            <PrimaryBtn>Contact Now</PrimaryBtn>
+          </Link>
+
           <div className="md:hidden">
             <Menu onClick={onClickMobileMenu} />
           </div>
